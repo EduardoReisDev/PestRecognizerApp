1fa2c6d11427c11940dd0d7a716b6ce2e95ddcf0
Eduardo Reis <eduardoreisdev@gmail.com>
2023-10-01 19:23:08 -0300

    Update ApiKeys.cs

--- PestRecognizerApp/Constants/ApiKeys.cs
+++ PestRecognizerApp/Constants/ApiKeys.cs
@@ -2,9 +2,9 @@
 {
     public static class ApiKeys
     {
-        public static string CustomVisionEndPoint => "https://pestrecognizer-prediction.cognitiveservices.azure.com/";
-        public static string PredictionKey => "b97c055bca8e4b47b7ba5fa4a7af4731";
-        public static string ProjectId => "78dc521e-049d-4046-9388-94a0e8ef7be5";
-        public static string PublishedName => "Iteration2";
+        public static string CustomVisionEndPoint => "";
+        public static string PredictionKey => "";
+        public static string ProjectId => "";
+        public static string PublishedName => "";
     }
 }
